c48b292f478d2e4a84ade5d74c8e6054ce7071bb
TicianoFilho <ticianofilho@gmail.com>
2023-04-21 12:41:03 -0300

    create project and add header and button components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash';
+  message: string = 'angular-crash';
+  value: boolean = false;
 }
