8ddedf186e0a6f04f0d347075a45a04f697fa93d
TicianoFilho <ticianofilho@gmail.com>
2023-04-21 19:55:15 -0300

    feature: add taskService and add Observable in it

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+  @Input() text: string = "";
+  @Input() color: String = "";
+  @Output() btnClick = new EventEmitter();
+
+  constructor() {
+
+  }
+
+  onClick() {
+    this.btnClick.emit();
+  }
+
+}
