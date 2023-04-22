9010cedaccf4ff1f8668f585c53352ef8312ad09
TicianoFilho <ticianofilho@gmail.com>
2023-04-22 12:56:37 -0300

    add two way binding for add task form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent {
+  description: string = "";
+  day: string = "";
+  reminder: boolean = false;
 
 }
